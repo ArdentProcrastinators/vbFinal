de1b3343ec5d1994dc70fe8cdd3f882d44ba69c7
BuildTools <unconfigured@null.spigotmc.org>
2016-04-27 16:55:58 -0400

    Ez change

--- Card Game/Card Game/Form1.Designer.vb
+++ Card Game/Card Game/Form1.Designer.vb
@@ -26,21 +26,25 @@ Partial Class Form1
         Me.lblTestP = New System.Windows.Forms.Label()
         Me.lblTestT = New System.Windows.Forms.Label()
         Me.lblNumCreatures = New System.Windows.Forms.Label()
+        Me.btnViewHand = New System.Windows.Forms.Button()
+        Me.grpHand = New System.Windows.Forms.GroupBox()
         CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'PictureBox1
         '
-        Me.PictureBox1.Location = New System.Drawing.Point(0, 0)
+        Me.PictureBox1.Image = Global.WindowsApplication1.My.Resources.Resources.Ardent_Procrastinor
+        Me.PictureBox1.Location = New System.Drawing.Point(32, 12)
         Me.PictureBox1.Name = "PictureBox1"
         Me.PictureBox1.Size = New System.Drawing.Size(100, 50)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
         Me.PictureBox1.TabIndex = 0
         Me.PictureBox1.TabStop = False
         '
         'lblTestP
         '
         Me.lblTestP.AutoSize = True
-        Me.lblTestP.Location = New System.Drawing.Point(61, 187)
+        Me.lblTestP.Location = New System.Drawing.Point(332, 9)
         Me.lblTestP.Name = "lblTestP"
         Me.lblTestP.Size = New System.Drawing.Size(39, 13)
         Me.lblTestP.TabIndex = 1
@@ -49,7 +53,7 @@ Partial Class Form1
         'lblTestT
         '
         Me.lblTestT.AutoSize = True
-        Me.lblTestT.Location = New System.Drawing.Point(167, 199)
+        Me.lblTestT.Location = New System.Drawing.Point(438, 21)
         Me.lblTestT.Name = "lblTestT"
         Me.lblTestT.Size = New System.Drawing.Size(39, 13)
         Me.lblTestT.TabIndex = 2
@@ -58,17 +62,37 @@ Partial Class Form1
         'lblNumCreatures
         '
         Me.lblNumCreatures.AutoSize = True
-        Me.lblNumCreatures.Location = New System.Drawing.Point(128, 239)
+        Me.lblNumCreatures.Location = New System.Drawing.Point(399, 61)
         Me.lblNumCreatures.Name = "lblNumCreatures"
         Me.lblNumCreatures.Size = New System.Drawing.Size(39, 13)
         Me.lblNumCreatures.TabIndex = 3
         Me.lblNumCreatures.Text = "Label1"
         '
+        'btnViewHand
+        '
+        Me.btnViewHand.Location = New System.Drawing.Point(179, 21)
+        Me.btnViewHand.Name = "btnViewHand"
+        Me.btnViewHand.Size = New System.Drawing.Size(75, 23)
+        Me.btnViewHand.TabIndex = 4
+        Me.btnViewHand.Text = "View hand"
+        Me.btnViewHand.UseVisualStyleBackColor = True
+        '
+        'grpHand
+        '
+        Me.grpHand.Location = New System.Drawing.Point(58, 340)
+        Me.grpHand.Name = "grpHand"
+        Me.grpHand.Size = New System.Drawing.Size(1185, 287)
+        Me.grpHand.TabIndex = 5
+        Me.grpHand.TabStop = False
+        Me.grpHand.Text = "Hand"
+        '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(284, 261)
+        Me.ClientSize = New System.Drawing.Size(1297, 639)
+        Me.Controls.Add(Me.grpHand)
+        Me.Controls.Add(Me.btnViewHand)
         Me.Controls.Add(Me.lblNumCreatures)
         Me.Controls.Add(Me.lblTestT)
         Me.Controls.Add(Me.lblTestP)
@@ -85,4 +109,6 @@ Partial Class Form1
     Friend WithEvents lblTestP As Label
     Friend WithEvents lblTestT As Label
     Friend WithEvents lblNumCreatures As Label
+    Friend WithEvents btnViewHand As Button
+    Friend WithEvents grpHand As GroupBox
 End Class
