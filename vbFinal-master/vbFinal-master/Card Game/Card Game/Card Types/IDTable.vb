808761e2bb07d4bb20cede4ad5335053bfce68b5
RetroWraith <maxwell.w.potter@gmail.com>
2016-05-11 09:20:04 -0400

    Added spooky stuff, added creature centering
    Lowered card scale because cards were too large

---
+++ vbFinal-master/vbFinal-master/Card Game/Card Game/Card Types/IDTable.vb
@@ -0,0 +1,224 @@
+﻿Public Class IDTable
+    Public Shared Function IDPower(ID As Integer) As Integer
+        'Retuns power of creature from ID
+        Select Case ID
+            Case 1
+                Return 2
+            Case 2
+                Return 2
+            Case 3
+                Return 3
+            Case 4
+                Return 4
+            Case 5
+                Return 5
+            Case Else
+                'Error return, returned if ID does not exist
+                Return 100
+        End Select
+    End Function
+
+    Public Shared Function IDHealth(ID As Integer) As Integer
+        'Retuns Health of creature from ID
+        Select Case ID
+            Case 1
+                Return 2
+            Case 2
+                Return 2
+            Case 3
+                Return 4
+            Case 4
+                Return 5
+            Case 5
+                Return 6
+            Case Else
+                'Error return, returned if ID does not exist
+                Return 100
+        End Select
+    End Function
+
+    Public Shared Function IDAbility(c As Card) As Integer
+        Select Case c.ID
+            Case Is = 1
+                If Form1.NeedTarget = True Then
+                    'Confirms this is target of ability
+                    If MsgBox("Would you like to give this creature -1/-0?", MsgBoxStyle.YesNo) = MsgBoxResult.Yes Then
+                        'Applies debuff and stops ability process
+                        Form1.Target.Buff(-1, 0)
+                        Form1.NeedTarget = False
+                    End If
+                ElseIf MsgBox("Would you like to use this card's ability?", MsgBoxStyle.YesNo) = MsgBoxResult.Yes Then
+                    'Says a target is needed if ability is used
+                    Form1.NeedTarget = True
+                    Form1.IDSearchingForTarget = c.ID
+                End If
+                Return 1
+            Case Is = 2
+                '//ABILITY TEMPLATE//
+                'If a target is being searched for
+                If Form1.NeedTarget = True Then
+                    'Confirms this is target of ability
+                    If MsgBox("Would you like to [list ability]", MsgBoxStyle.YesNo) = MsgBoxResult.Yes Then
+                        'Ability code
+                        'Disables target search
+                        Form1.NeedTarget = False
+                    End If
+                    'If no target is needed currently
+                ElseIf MsgBox("Would you like to use this card's ability?", MsgBoxStyle.YesNo) = MsgBoxResult.Yes Then
+                    'Says a target is needed if ability is used, starts search for target
+                    Form1.NeedTarget = True
+                    Form1.IDSearchingForTarget = c.ID
+                End If
+            Case Is = 3
+                '//LAND TEMPLATE//
+                'Ez
+                If c.used = False Then
+                    If c.tapped = False Then
+                        c.used = True
+                        Form1.AddMana("blue")
+                        c.BackgroundImage = My.Resources.islandt
+                    End If
+                Else
+                    If c.tapped = False Then
+                            c.used = False
+                            Form1.RemMana("blue")
+                            c.BackgroundImage = My.Resources.island
+                        End If
+                    End If
+
+                    Case 4
+                Return 0
+            Case 5
+                Return 0
+        End Select
+        Return 0
+    End Function
+
+    Public Shared Sub IDUseAbility(ID As Integer)
+        Select Case ID
+            Case 1
+                Form1.Target.Buff(-1, 0)
+            Case 2
+
+            Case 3
+
+            Case Is = 4
+
+            Case Is = 5
+
+        End Select
+
+    End Sub
+
+    Public Shared Function IDImage(C As Card) As Image
+        'Sets the new Card's Image to the card played
+        Select Case C.ID
+            Case 1
+                Return My.Resources.NoobMemer
+            Case 2
+                Return My.Resources.Ardent_Procrastinor
+            Case 3
+                If C.tapped = False Then
+                    Return My.Resources.island
+                Else
+                    Return My.Resources.islandtu
+                End If
+            Case 4
+
+            Case 5
+
+            Case 31
+
+        End Select
+    End Function
+
+    Public Shared Sub PlayCard(c As Card)
+
+        Select Case c.ID
+            Case 1
+                If PayMana(c) Then
+
+                End If
+            Case 2
+
+            Case 3
+                If Form1.landPlayed <= Form1.landMax Then
+
+                    Form1.handInfo.RemoveAt(Form1.handInfo.IndexOf(c))
+                    Form1.landInfo.Add(c)
+                    c.partOfHand = False
+                    Form1.UpdateHand()
+                    c.Name = "blue" & Form1.landInfo.Count
+                    Form1.Controls(c.Name).Top = -50 * Form1.landInfo.Count + Form1.Height / 4
+                    Form1.Controls(c.Name).Left = 100
+                    Form1.landPlayed += 1
+
+                End If
+
+        End Select
+
+    End Sub
+
+    Public Shared Function PayMana(iC As Card) As Boolean
+
+        'Ill comment this eventually
+        If Form1.manaPool.Count <> 0 Then
+
+            Dim testList As New List(Of String)
+            Static counter As Integer
+
+            For x As Integer = 1 To Form1.manaPool.Count
+                testList.Add(Form1.manaPool(x - 1))
+            Next
+
+
+            For I As Integer = 0 To iC.manaCost.Count - 1
+
+                counter = 0
+                Dim result As Integer = 0
+
+                Do Until result <> 0
+
+                    If testList.Count = 0 Then Return False
+
+                    If iC.manaCost(I) = testList(counter) Or iC.manaCost(I) = "any" Then
+
+                        testList.Remove(testList(counter))
+                        result = 1
+
+                    ElseIf counter = testList.Count - 1
+                        Return False
+                    End If
+
+                    counter += 1
+
+                Loop
+
+            Next
+
+            Form1.TMC(iC)
+            Form1.ManaLabel()
+            Return True
+
+        End If
+
+    End Function
+
+    Public Shared Sub SetMana(c As Card)
+
+        Select Case c.ID
+            Case 1
+                'Add anys last
+                c.manaCost.Add("blue")
+                c.manaCost.Add("any")
+            Case 2
+
+            Case 3
+                'Cost on lands is what they give
+                c.manaCost.Add("blue")
+
+        End Select
+
+    End Sub
+
+End Class
---
+++ vbFinal-master/vbFinal-master/Card Game/Card Game/Form1.Designer.vb
@@ -0,0 +1,170 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.lblTestP = New System.Windows.Forms.Label()
+        Me.lblTestT = New System.Windows.Forms.Label()
+        Me.lblNumCreatures = New System.Windows.Forms.Label()
+        Me.btnViewHand = New System.Windows.Forms.Button()
+        Me.tmrUpdater = New System.Windows.Forms.Timer(Me.components)
+        Me.btnMulligan = New System.Windows.Forms.Button()
+        Me.btnConfirm = New System.Windows.Forms.Button()
+        Me.lblMana = New System.Windows.Forms.Label()
+        Me.lblManaStatus = New System.Windows.Forms.Label()
+        Me.btnTS = New System.Windows.Forms.Button()
+        Me.btnSpawn = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'lblTestP
+        '
+        Me.lblTestP.AutoSize = True
+        Me.lblTestP.Location = New System.Drawing.Point(353, 26)
+        Me.lblTestP.Name = "lblTestP"
+        Me.lblTestP.Size = New System.Drawing.Size(39, 13)
+        Me.lblTestP.TabIndex = 1
+        Me.lblTestP.Text = "Label1"
+        '
+        'lblTestT
+        '
+        Me.lblTestT.AutoSize = True
+        Me.lblTestT.Location = New System.Drawing.Point(438, 21)
+        Me.lblTestT.Name = "lblTestT"
+        Me.lblTestT.Size = New System.Drawing.Size(39, 13)
+        Me.lblTestT.TabIndex = 2
+        Me.lblTestT.Text = "Label2"
+        '
+        'lblNumCreatures
+        '
+        Me.lblNumCreatures.AutoSize = True
+        Me.lblNumCreatures.Image = CType(resources.GetObject("lblNumCreatures.Image"), System.Drawing.Image)
+        Me.lblNumCreatures.Location = New System.Drawing.Point(399, 61)
+        Me.lblNumCreatures.Name = "lblNumCreatures"
+        Me.lblNumCreatures.Size = New System.Drawing.Size(39, 13)
+        Me.lblNumCreatures.TabIndex = 3
+        Me.lblNumCreatures.Text = "Label1"
+        '
+        'btnViewHand
+        '
+        Me.btnViewHand.Location = New System.Drawing.Point(211, 21)
+        Me.btnViewHand.Name = "btnViewHand"
+        Me.btnViewHand.Size = New System.Drawing.Size(75, 23)
+        Me.btnViewHand.TabIndex = 4
+        Me.btnViewHand.Text = "View hand"
+        Me.btnViewHand.UseVisualStyleBackColor = True
+        '
+        'tmrUpdater
+        '
+        Me.tmrUpdater.Enabled = True
+        Me.tmrUpdater.Interval = 10
+        '
+        'btnMulligan
+        '
+        Me.btnMulligan.Location = New System.Drawing.Point(567, 21)
+        Me.btnMulligan.Name = "btnMulligan"
+        Me.btnMulligan.Size = New System.Drawing.Size(75, 23)
+        Me.btnMulligan.TabIndex = 5
+        Me.btnMulligan.Text = "Mulligan"
+        Me.btnMulligan.UseVisualStyleBackColor = True
+        '
+        'btnConfirm
+        '
+        Me.btnConfirm.Location = New System.Drawing.Point(679, 20)
+        Me.btnConfirm.Name = "btnConfirm"
+        Me.btnConfirm.Size = New System.Drawing.Size(75, 23)
+        Me.btnConfirm.TabIndex = 6
+        Me.btnConfirm.Text = "Confirm Hand"
+        Me.btnConfirm.UseVisualStyleBackColor = True
+        '
+        'lblMana
+        '
+        Me.lblMana.AutoSize = True
+        Me.lblMana.Location = New System.Drawing.Point(1133, 42)
+        Me.lblMana.Name = "lblMana"
+        Me.lblMana.Size = New System.Drawing.Size(37, 13)
+        Me.lblMana.TabIndex = 9
+        Me.lblMana.Text = "Mana:"
+        '
+        'lblManaStatus
+        '
+        Me.lblManaStatus.AutoSize = True
+        Me.lblManaStatus.Location = New System.Drawing.Point(524, 108)
+        Me.lblManaStatus.Name = "lblManaStatus"
+        Me.lblManaStatus.Size = New System.Drawing.Size(0, 13)
+        Me.lblManaStatus.TabIndex = 10
+        '
+        'btnTS
+        '
+        Me.btnTS.Location = New System.Drawing.Point(557, 98)
+        Me.btnTS.Name = "btnTS"
+        Me.btnTS.Size = New System.Drawing.Size(75, 23)
+        Me.btnTS.TabIndex = 11
+        Me.btnTS.Text = "turn start"
+        Me.btnTS.UseVisualStyleBackColor = True
+        '
+        'btnSpawn
+        '
+        Me.btnSpawn.Location = New System.Drawing.Point(711, 103)
+        Me.btnSpawn.Name = "btnSpawn"
+        Me.btnSpawn.Size = New System.Drawing.Size(75, 23)
+        Me.btnSpawn.TabIndex = 12
+        Me.btnSpawn.Text = "Spawn"
+        Me.btnSpawn.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackgroundImage = Global.WindowsApplication1.My.Resources.Resources.Untitled_drawing
+        Me.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.ClientSize = New System.Drawing.Size(1297, 639)
+        Me.Controls.Add(Me.btnSpawn)
+        Me.Controls.Add(Me.btnTS)
+        Me.Controls.Add(Me.lblManaStatus)
+        Me.Controls.Add(Me.lblMana)
+        Me.Controls.Add(Me.btnConfirm)
+        Me.Controls.Add(Me.btnMulligan)
+        Me.Controls.Add(Me.btnViewHand)
+        Me.Controls.Add(Me.lblNumCreatures)
+        Me.Controls.Add(Me.lblTestT)
+        Me.Controls.Add(Me.lblTestP)
+        Me.DoubleBuffered = True
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents lblTestP As Label
+    Friend WithEvents lblTestT As Label
+    Friend WithEvents lblNumCreatures As Label
+    Friend WithEvents btnViewHand As Button
+    Friend WithEvents tmrUpdater As Timer
+    Friend WithEvents btnMulligan As Button
+    Friend WithEvents btnConfirm As Button
+    Friend WithEvents lblMana As Label
+    Friend WithEvents lblManaStatus As Label
+    Friend WithEvents btnTS As Button
+    Friend WithEvents btnSpawn As System.Windows.Forms.Button
+End Class
